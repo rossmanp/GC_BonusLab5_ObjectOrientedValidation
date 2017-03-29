9d3bebe1c14031ee9c4a22aaa7cd41e08471119a
Pete Rossman <pete.rossman@gmail.com>
2017-03-29 13:41:21 -0400

    First commit

---
+++ GC_BonusLab5_ObjectOrientedValidation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_BonusLab5_ObjectOrientedValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Validation Tester Application!");
+            Console.WriteLine("\nInt Test");
+            Console.WriteLine("Enter an integer between -100 and 100: ");
+            Console.WriteLine("\nDouble Test");
+            Console.WriteLine("Enter any number between -100 and 100: ");
+            Console.ReadLine();
+        }
+    }
+}
