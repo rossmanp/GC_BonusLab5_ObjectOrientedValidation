5293231f0bd2f927370d5c7de3476bc15259244b
Pete Rossman <pete.rossman@gmail.com>
2017-03-30 10:03:12 -0400

    Add MyValidator class

---
+++ GC_BonusLab5_ObjectOrientedValidation/MyValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_BonusLab5_ObjectOrientedValidation
+{
+    class MyValidator
+    {
+    }
+}
